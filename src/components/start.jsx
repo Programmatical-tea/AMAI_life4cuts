d0ad34143454edf11c9adc671c709bb94623fdf5
Programmatical-tea <davidjeon0208@gmail.com>
2022-05-18 01:22:02 +0900

    webcam

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Webcam from "react-webcam";
 
-
-  
+ 
 function Start() {
+
+    const webcamRef = React.useRef(null);
+    const [imgSrc, setImgSrc] = React.useState(null);
+    const capture = React.useCallback(() => {
+      const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+    }, [webcamRef, setImgSrc]);
+
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: "user"
+    }
+
     let homestyle = {
         display:"flex",
         "flex-direction":"column",
@@ -13,11 +27,20 @@ function Start() {
 
     return (
       <div style={homestyle}>
-        <p class="start_block" id="start_amai">#크사네컷</p>
-        <p class="start_block" id="start_info">준비되면 시작해주세요!</p>
-        <p class="start_block" id="start_order">&nbsp;</p>
-        <canvas class="start_block" width="960px" height="540px" id="start_cnv"></canvas>
-        <p class="start_block" id="start_icon">
+        <p className="start_block" id="start_amai">#크사네컷</p>
+        <p className="start_block" id="start_info">준비되면 시작해주세요!</p>
+        <p className="start_block" id="start_order">&nbsp;</p>
+        <div className = "start_webcam">
+          <Webcam
+            audio={false}
+            ref={webcamRef}
+            width={960}
+            height={540}
+            screenshotFormat="image/jpeg"
+            videoConstraints = {videoConstraints}
+          />
+        </div>
+        <p className="start_block" id="start_icon">
             <NavLink to = '/photo'>📸</NavLink>
         </p>
       </div>
